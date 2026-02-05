e49bb141c951fc3412f82785cc983396b66a325f
nahomberaki <240136419@aston.ac.uk>
2026-02-05 21:08:28 +0000

    Added ability to Remove Coupons from a cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -102,4 +102,11 @@ class CartController extends Controller
             ]);
         }
     }
+
+    public function remove_coupon_code()
+    {
+        Session::forget('coupon');
+        Session::forget('discounts');
+        return back()->with('success','Coupon has been removed successfully');
+    }
 }
