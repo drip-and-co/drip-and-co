ed79fd1f93df6c0cdad17874a0e0d84aaab0ebd3
Ishita <240169990@aston.ac.uk>
2026-03-02 18:37:16 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Auth::routes();
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
 Route::get('/shop/{product_slug}', [ShopController::class, 'product_details'])->name('shop.product.details');
+Route::post('/product/review', [ShopController::class, 'store_review'])->name('product.review.store');
 
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add', [CartController::class, 'add_to_cart'])->name('cart.add');
